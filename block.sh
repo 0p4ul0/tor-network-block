d3aeef1e8f290d75d1b3f8c82349787974aa5d10
Paulo Roberto <paulosilva@cvccorp.com.br>
2020-06-02 01:32:27 -0300

    Implements script.

--- block.sh
+++ block.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+URL_TOR_CHECK="https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip="
+HOSTS=$(curl -L -X GET $URL_TOR_CHECK)
+
+for host in $HOSTS
+do
+   iptables -A INPUT -s $host -j DROP
+done
